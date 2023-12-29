8c9ab449198f68f0901d191f84cea2d77139da6f
mohycs <ma1097@scarletmail.rutgers.edu>
2023-12-29 01:05:10 -0500

    update config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "export",
   images: {
-    domains: ['miro.medium.com'],
+    unoptimized: true,
   },
   experimental: {
     appDir: true,
