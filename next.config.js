c2f99be6a703b9b72a04d80dae0ab1491ebb7273
mohycs <ma1097@scarletmail.rutgers.edu>
2023-12-29 01:37:12 -0500

    Deprecate doomains

--- next.config.js
+++ next.config.js
@@ -1,13 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['miro.medium.com'],
     remotePatterns: [
       {
         protocol: 'https',
         hostname: 'miro.medium.com',
-        port: '',
-        pathname: '/',
+        pathname: '**',
       },
     ]
   },
