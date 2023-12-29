43ce92268ee2fff149abd0825efd868731f42b94
mohycs <ma1097@scarletmail.rutgers.edu>
2023-12-29 00:25:14 -0500

    Update path map

--- next.config.js
+++ next.config.js
@@ -8,10 +8,14 @@ const nextConfig = {
   },
   exportPathMap: async function (
     defaultPathMap,
-    { home }
+    { dev, dir, outDir, distDir, buildId }
   ) {
     return {
       '/': { page: '/' },
+      '/about': { page: '/about' },
+      '/p/hello-nextjs': { page: '/post', query: { title: 'hello-nextjs' } },
+      '/p/learn-nextjs': { page: '/post', query: { title: 'learn-nextjs' } },
+      '/p/deploy-nextjs': { page: '/post', query: { title: 'deploy-nextjs' } },
     }
   },
 }
